4bdc6bac47bacebb91e0b993d9e3bc1a0360b2df
leo <leo000111444@gmail.com>
2022-02-22 17:43:20 +0800

    setup herohu

--- api_basic/views.py
+++ api_basic/views.py
@@ -1,4 +1,4 @@
-from turtle import st
+# from turtle import st
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
